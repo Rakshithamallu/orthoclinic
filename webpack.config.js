227d8b417729f1effef51bea582b030ac926d6d5
Rakshithamallu <rakshithamallu@github.com>
2026-09-16 20:53:09 +0530

    Add GitHub Pages deployment config (gh-pages, 404.html, publicPath fix)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'static/js/[name].[contenthash:8].js',
-    publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/orthoclinic/' : '/',
     clean: true,
   },
   resolve: {
