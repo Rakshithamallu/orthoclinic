301eaf28290ce671171697f61548dcefb7b93da3
Rakshithamallu <rakshithamallu@github.com>
2026-09-17 21:55:01 +0530

    Update Home page to only display Hero, Stats, WhyChooseUs, FAQ, AppointmentCTA, and Contact

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Stats from '../components/Stats';
-import AboutDoctor from '../components/AboutDoctor';
-import EducationTimeline from '../components/EducationTimeline';
-import Fellowships from '../components/Fellowships';
-import Conditions from '../components/Conditions';
-import Treatments from '../components/Treatments';
-import Facilities from '../components/Facilities';
 import WhyChooseUs from '../components/WhyChooseUs';
-import Research from '../components/Research';
-import PatientJourney from '../components/PatientJourney';
-import Blog from '../components/Blog';
-import AppointmentCTA from '../components/AppointmentCTA';
 import FAQ from '../components/FAQ';
+import AppointmentCTA from '../components/AppointmentCTA';
 import Contact from '../components/Contact';
 
 const Home = ({ onOpenAppointmentModal }) => {
@@ -20,18 +11,9 @@ const Home = ({ onOpenAppointmentModal }) => {
     <div className="home-page">
       <Hero onOpenAppointmentModal={onOpenAppointmentModal} />
       <Stats />
-      <AboutDoctor onOpenAppointmentModal={onOpenAppointmentModal} />
-      <EducationTimeline />
-      <Fellowships />
-      <Conditions onOpenAppointmentModal={onOpenAppointmentModal} />
-      <Treatments onOpenAppointmentModal={onOpenAppointmentModal} />
-      <Facilities />
       <WhyChooseUs />
-      <Research />
-      <PatientJourney />
-      <Blog onOpenAppointmentModal={onOpenAppointmentModal} />
-      <AppointmentCTA onOpenAppointmentModal={onOpenAppointmentModal} />
       <FAQ />
+      <AppointmentCTA onOpenAppointmentModal={onOpenAppointmentModal} />
       <Contact />
     </div>
   );
